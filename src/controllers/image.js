76d46d31ce0423407b15f73b0b28efeeb57f8eed
tommy <tommysolano18@hotmail.com>
2021-09-09 04:09:54 -0500

    upload image

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,11 +1,23 @@
+const path = require('path')
+const { randomNumber} = require('../helpers/libs')
+const fs = require('fs-extra')
 const ctrl = {}
 
 ctrl.index = (req, res) => {
 
 }
 
-ctrl.create = (req, res) => {
+ctrl.create = async (req, res) => {
+    const imgURL = randomNumber()
+    const imageTempPath = req.file.path
+    const ext = path.extname(req.file.originalname).toLowerCase()
+    const targetPath = path.resolve(`src/public/upload/${imgURL}${ext}`)
     
+    if(ext === '.jpg' || ext === '.png' || ext === '.jpeg' || ext === '.gif'){
+        await fs.rename(imageTempPath, targetPath)
+    }
+
+    res.send("works")
 }
 
 ctrl.like = (req, res) => {
