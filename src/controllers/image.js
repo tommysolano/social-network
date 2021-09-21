297a79ad2639a1a6a6663cc620d924132ba73da1
tommy <tommysolano18@hotmail.com>
2021-09-21 02:21:59 -0500

    post comment

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,7 +1,8 @@
 const path = require('path')
 const { randomNumber } = require('../helpers/libs')
 const fs = require('fs-extra')
-const { Image } = require('../models/index')
+const { Image, Comment } = require('../models/index')
+const md5 = require('md5')
 const ctrl = {}
 
 ctrl.index = async (req, res) => {
@@ -44,8 +45,15 @@ ctrl.like = (req, res) => {
 
 }
 
-ctrl.comment = (req, res) => {
-
+ctrl.comment = async (req, res) => {
+    const image = await Image.findOne({filename: {$regex: req.params.image_id}})
+    if (image) {
+        const newComment = new Comment(req.body)
+        newComment.gravatar = md5(newComment.email)
+        newComment.image_id = image._id
+        newComment.save()
+        res.redirect('/images/'+image.uniqueId)
+    }
 }
 
 ctrl.remove = (req, res) => {
---
+++ src/models/comment.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+const { ObjectId } = Schema
+
+const CommentSchema = new Schema ({
+    image_id: { type: ObjectId },
+    email: { type: String},
+    name: { type: String},
+    gravatar: { type: String },
+    comment: { type: String},
+    timestamp: { type: Date, default: Date.now}
+})
+
+module.exports = model("Comment", CommentSchema)
