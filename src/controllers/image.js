9b2948dc9cc62ab4fa137936d82b45ad8fb173e9
tommy <tommysolano18@hotmail.com>
2021-09-21 21:07:06 -0500

    adding likes

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -51,8 +51,13 @@ ctrl.create = (req, res) => {
     saveImage()
 }
 
-ctrl.like = (req, res) => {
-
+ctrl.like = async (req, res) => {
+    const image = await Image.findOne({filename: {$regex: req.params.image_id}})
+    if (image) {
+        image.likes = image.likes + 1
+        await image.save()
+        res.json({likes: image.likes})
+    }  
 }
 
 ctrl.comment = async (req, res) => {
